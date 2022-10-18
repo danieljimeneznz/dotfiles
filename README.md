eb24d8871460e4cb6b6e17665ecfd7c18b7c9dc0
Daniel Jimenez <devildan.knex@gmail.com>
2022-10-18 14:49:52 +1300

    fix: Add readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# dotfiles
+
+Dotfiles for environment setup, if on POSIX environment install zsh and startship.rs, then run:
+
+```
+./run.sh
+source ~/.zshrc
+```
--- run.sh
+++ run.sh
@@ -1,9 +1,8 @@
+#!/bin/bash
+
 # Copy zshrc file
 cp .zshrc ~/.zshrc
 
 # Setup starship
 mkdir -p ~/.config
 cp .config/starship.toml ~/.config/starship.toml
-
-# Reload shell
-source ~/.zshrc
